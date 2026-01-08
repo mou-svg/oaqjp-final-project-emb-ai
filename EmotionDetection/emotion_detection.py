d44b9579524ec567199762ff4a8aaa122e0318cd
mou-svg <mousumi.sr@gmail.com>
2026-01-08 13:16:23 -0500

    final

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,15 +6,24 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
     formatted_response = json.loads(response.text)
+ 
     # Extracting sentiment label and score from the response
-    
-    emotions = formatted_response['emotionPredictions'][0]['emotion']
-    anger_score = emotions['anger']
-    disgust_score = emotions['disgust']
-    fear_score = emotions['fear']
-    joy_score = emotions['joy']
-    sadness_score = emotions['sadness']
-    max_emotion = max(emotions, key = emotions.get)
+    if response.status_code == 200:
+        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        anger_score = emotions['anger']
+        disgust_score = emotions['disgust']
+        fear_score = emotions['fear']
+        joy_score = emotions['joy']
+        sadness_score = emotions['sadness']
+        max_emotion = max(emotions, key = emotions.get)
+    elif response.status_code == 400:
+        emotions = None
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+        max_emotion = None
      
     # Returning a dictionary containing sentiment analysis results
     return {'anger': anger_score, 'disgust': disgust_score, 'fear': fear_score, 'joy': joy_score, 'sadness': sadness_score, 'dominant_emotion': max_emotion}
--- server.py
+++ server.py
@@ -1,24 +1,34 @@
-from flask import Flask
+#importing libraries
+from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
+#Flask initiation
 app = Flask("Emotion Detector")
 
+#adding routes
 @app.route("/emotionDetector")
 def emot_detector():
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
-    # Pass the text to the sentiment_analyzer function and store the response
-    response = emot_detector(text_to_analyze)
-    
+    # Pass the text to the emot_detector function and store the response
+    response = emotion_detector(text_to_analyze)
+    #response parsing
     anger = response['anger']
     disgust = response['disgust']
     fear = response['fear']
     joy = response['joy']
     sadness = response['sadness']
     dominant_emotion = response['dominant_emotion']
+    #error handling
+    if dominant_emotion is None:
+        return "Invalid input! Try again."
 
-    return response
-    # Return a formatted string with the sentiment label and score
-    #return "For the given statement, the system response is {} with a score of {}.".format(score)
-    
-    
+    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}." .format(anger, disgust,fear, joy, sadness, dominant_emotion)   
+
+#rendermodule
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
